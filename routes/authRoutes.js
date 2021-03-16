c3812e9269559fcc675ee126429b6154996d81a6
Truong Dao <ton.truong.dao@outlook.com>
2021-03-16 17:07:45 -0400

    lesson48

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,6 +15,13 @@ module.exports = (app) => {
     )
 
     app.get(
+        '/api/logout', 
+        (req, res) => {
+            req.logout();
+            res.send(req.user);
+    })
+
+    app.get(
         '/api/current_user',
         (req, res) => {
             res.send(req.user);
