3129ca59f05507de422c5a674fbd47a09b5e4feb
Truong Dao <ton.truong.dao@outlook.com>
2021-03-23 13:11:56 -0400

    lesson77

---
+++ .notes/section7 client dev/lesson77.md
@@ -0,0 +1,1 @@
+- Did "Router" boilerplate in App.js.
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      Hi there! 
+      <BrowserRouter>
+        <div>
+          <Route exact path ='/' component={}/>
+          <Route path='/surveys' component={}/>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
