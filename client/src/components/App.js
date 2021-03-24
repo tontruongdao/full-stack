436c7cc915c1be411dfc641855fd1425ef128840
Truong Dao <ton.truong.dao@outlook.com>
2021-03-24 11:35:05 -0400

    lesson85

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Header from './Header'
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
@@ -9,9 +12,9 @@ const App = () => {
       <BrowserRouter>
         <div>
           <Header/>
-          {/* <Route exact path ='/' component={}/>
+          <Route exact path ='/' component={}/>
           <Route exact path='/surveys' component={}/>
-          <Route path='/surveys/new' component={}/> */}
+          <Route path='/surveys/new' component={}/>
         </div>
       </BrowserRouter>
     </div>
