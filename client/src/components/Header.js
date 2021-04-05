57ea8bf8461f14a3db7e5e7c2c2eba77e286eecb
Truong Dao <ton.truong.dao@outlook.com>
2021-04-05 11:17:44 -0400

    lesson105

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,11 +17,12 @@ class Header extends Component {
                     </li>
                 )
             default:
-                return (
-                    <li>
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="2">
                         <a href="/api/logout">Logout</a>
                     </li>
-                )
+                ]                
         }
     }
 
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,13 +4,19 @@ import StripeCheckout from 'react-stripe-checkout'
 class Payments extends Component {
 
     render() {
-        debugger;
+
         return(
             <StripeCheckout
+                name="Emailz"
+                description="$5 for 5 email credits"
                 amount={500}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}            
-            />
+            >
+                <button className="btn">
+                    Add Credits
+                </button>
+            </StripeCheckout>
         )    
     }
 }
