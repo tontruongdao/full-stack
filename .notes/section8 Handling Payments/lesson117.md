65532b1358923594b1dea4b6b042ae261ec02c7f
Truong Dao <ton.truong.dao@outlook.com>
2021-04-05 14:36:32 -0400

    lesson117

---
+++ .notes/section8 Handling Payments/lesson117.md
@@ -0,0 +1,2 @@
+- Created new middleware folder and included "requireLogin" middleware.
+- Rendered credit amount in Header(FE).
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,8 +3,10 @@ const stripe = require('stripe')(
     keys.stripeSecretKey
 )
 
+const requireLogin = require('../middlewares/requireLogin')
+
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
