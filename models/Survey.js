0f45d99f328f63716a42bdfc7148d9df5b7c0a3f
Truong Dao <ton.truong.dao@outlook.com>
2021-04-06 16:22:42 -0400

    lesson131

--- models/Survey.js
+++ models/Survey.js
@@ -8,7 +8,10 @@ const surveySchema = newSchema({
     subject: String,
     recipients: [RecipientSchema],
     yes: { type: Number, default: 0 },
-    no: { type: Number, default: 0 }
+    no: { type: Number, default: 0 },
+    _user: { type: Schema.Types.ObjectId, ref: "User" },
+    dateSent: Date,
+    lastResponded: Date
 })
 
 mongoose.model('survey', surveySchema);
