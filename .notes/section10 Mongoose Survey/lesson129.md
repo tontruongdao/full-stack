a7f639f4e3604652066a6adb49f083d583872a18
Truong Dao <ton.truong.dao@outlook.com>
2021-04-06 15:55:01 -0400

    lesson129

---
+++ .notes/section10 Mongoose Survey/lesson129.md
@@ -0,0 +1,1 @@
+- Added Recipient Schema to "Survey.js".
--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const RecipientSchema = require('./Recipient')
 const { Schema } = mongoose;
 
 const surveySchema = newSchema({
     title: String,
     body: String,
     subject: String,
-    recipients: [String],
+    recipients: [RecipientSchema],
     yes: { type: Number, default: 0 },
     no: { type: Number, default: 0 }
 })
